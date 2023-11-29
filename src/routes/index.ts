070297c71ae06146027d6c33f86a60f49552cdc4
Jason Smith <jason@amotivv.com>
2023-11-29 16:45:05 -0500

    Track dot files

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,7 +5,7 @@ import { metaRouter } from './meta.routes'
 export const parentRouter: Router = express.Router()
 
 const isDev = process.env.NODE_ENV === 'development'
-const host = isDev ? 'https://alb3lla.ngrok.io' : 'https://app.krat.es'
+const host = isDev ? 'https://alb3lla.ngrok.io' : 'https://krates-frontend-dev-glc8m.ondigitalocean.app'
 
 parentRouter.get('/', (req, res) => {
   res.redirect(301, `${host}`)
